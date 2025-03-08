b28520aa6cb2cb5fe10ea581ab6e35e87375152b
jiwon <iamkjw77@gmail.com>
2025-03-09 02:09:28 +0900

    fix: deploy.yml 파일 수정

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export', // Next.js를 정적 사이트로 변환
   basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   images: {
     unoptimized: true,
   },
