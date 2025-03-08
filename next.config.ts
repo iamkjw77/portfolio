f6507379f4bd5bc9150115d71149605815667981
jiwon <iamkjw77@gmail.com>
2025-03-09 02:18:20 +0900

    fix: next.config.ts 수정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,26 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', // Next.js를 정적 사이트로 변환
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: 'export',
+
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
   basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
   images: {
     unoptimized: true,
   },
