2e026d4d7073c2296a0670e5cdc0d00446071713
jiwon <iamkjw77@gmail.com>
2025-03-09 01:59:11 +0900

    fix: next.config.ts assetPrefix 삭제

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export', // Next.js를 정적 사이트로 변환
   basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
