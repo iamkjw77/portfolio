a7f74621ee90274b53d759e1b323f98b015e616f
jiwon <iamkjw77@gmail.com>
2025-03-26 21:32:48 +0900

    feat: 404 페이지 생성

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@
 const Error = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <p>프로젝트를 찾을 수 없습니다.</p>
+      <p>요청하신 페이지가 존재하지 않습니다.</p>
     </div>
   );
 };
